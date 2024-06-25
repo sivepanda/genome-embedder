13509890e7110ea510009bd3e55eecaf49b0a45e
sivepanda <siven-panda@omrf.org>
2024-06-25 17:25:23 -0500

    fix gitignore

--- covvec.py
+++ covvec.py
@@ -3,6 +3,25 @@ import pybedtools
 import numpy as np
 
 
+def each_file():
+    # Specify the directory path
+    directory_path = os.path.join(os.getcwd(), "tracks")
+    
+    all_data = []
+
+    # Iterate over each file in the directory
+    print("Reading files in ./tracks")
+    for filename in os.listdir(directory_path):
+        file_path = os.path.join(directory_path, filename)
+        if os.path.isfile(file_path):
+            print("Creating coverage vectors")
+            temp = create_coverage_vectors( feature_file = filename )
+            print(len(temp))
+            all_data.append(temp)
+
+    return np.array( all_data )
+
+
 # Returns an hashmap of bedtools objects for each of the inputted features. Pull data from UCSC if desired.
 def create_bedtools(genomic_features, base, genome, chrom, start, end):
     genomic_features_bedtools = {}
@@ -15,30 +34,27 @@ def create_bedtools(genomic_features, base, genome, chrom, start, end):
     return genomic_features_bedtools
 
 # Creates a vector containing the proportion of each window that is covered by a particular track
-def create_coverage_vectors( feature='', feature_file='', genome="hg38", base='tracks', window_size=1000 ):
+def create_coverage_vectors( feature='', feature_file='', genome="hg38", base='tracks', window_size=1000):
     if len( feature ) == 0 and len( feature_file ) == 0:
         raise Exception("You must provide an argument for either feature or feature_file")
     
+    # if os.path.isfile(file_path):
+
     if len( feature_file ) == 0:
         feature_file = feature + '.bed'
 
     windows = pybedtools.BedTool().window_maker(g=os.path.join(os.getcwd(), base, 'ref', genome + '.fa.fai') , w=window_size)
     feature = pybedtools.example_bedtool(os.path.join(os.getcwd(), base , feature_file))
-    
-
 
     overlap = windows.intersect(feature, c=True)
-    print(len(windows))
-
     coverage_vec = []
 
     for f in overlap:
-
         try:
             # for now, we'll omit the final sequences of each chromosome to allow for vectors to retain the same dimensionality
             # later, strategy could either be appending zeros to fill out the remainder of the window, or it could be adjusting 
             # window size to work with everything else, assuming it makes sense computationally + no prime lengths
-            if f <= 1000:
+            if f <= window_size:
                 n = f
                 coverage_vec.append(n / window_size)
         except:
--- train.py
+++ train.py
@@ -2,27 +2,11 @@ import os
 import numpy as np
 from covvec import create_coverage_vectors 
 from sklearn.model_selection import train_test_split
-
+import matplotlib.pyplot as plt
 import tensorflow as tf
 from tensorflow.keras.models import Model
 from tensorflow.keras.layers import Input, Dense
-def each_file():
-    # Specify the directory path
-    directory_path = os.path.join(os.getcwd(), "tracks")
-    
-    all_data = []
 
-    # Iterate over each file in the directory
-    for filename in os.listdir(directory_path):
-        file_path = os.path.join(directory_path, filename)
-        if os.path.isfile(file_path):
-            temp = create_coverage_vectors( feature_file = filename )
-            print(len(temp))
-            all_data.append(temp)
-        else:
-            print(f"Found directory: {filename}")
-
-    return np.array( all_data )
 
 data = each_file()
 x_train, x_test = train_test_split(data, test_size=0.2, random_state=42)
@@ -30,7 +14,7 @@ x_train, x_test = train_test_split(data, test_size=0.2, random_state=42)
 
 # Define the size of the encoding
 input_dim = x_train.shape[1]
-encoding_dim = 32  # Dimension of the latent space
+encoding_dim = 70 # Dimension of the latent space
 
 input_data = Input(shape=(input_dim,))
 
@@ -40,14 +24,12 @@ decoded = Dense(input_dim, activation='sigmoid')(encoded)
 
 autoencoder = Model(input_data, decoded)
 
-autoencoder.compile(optimizer='adam', loss='binary_crossentropy')
+autoencoder.compile(optimizer='adam', loss='mse')
 
 autoencoder.fit(x_train, x_train,
-                epochs=50,
-                batch_size=256,
+                epochs=100,
+                batch_size=128,
                 shuffle=True,
                 validation_data=(x_test, x_test))
 
-
-print(f'x_train shape: {x_train.shape}')
-print(f'x_test shape: {x_test.shape}')
+autoencoder.save('genome_encoder_alpha.keras')
