b9db22502d169636a1b89066186f3d61b890fc17
sivepanda <sivenpanda@icloud.com>
2024-07-18 14:47:57 -0500

    Implementing GenCovVec in C

--- compute_coverage/gcc.c
+++ compute_coverage/gcc.c
@@ -1,1 +1,22 @@
 #include <stdio.h>
+
+int square(int i) {
+	return i * i;
+}
+
+int listfn(float array[]) {
+    for (int i = 0; i < array.length; i++) {
+        printf(array[i]);
+    }
+
+}
+
+// for f in overlap:
+        // print( f[-1] )
+    // try:
+        // n = float( f[-1] )
+        // if n <= 1 and n >= 0:
+            // coverage_vec.append(n)
+    //  except:
+        // n = 0
+        // coverage_vec.append(n)
--- compute_coverage/generate_coverage_vectors.py
+++ compute_coverage/generate_coverage_vectors.py
@@ -1,1 +1,12 @@
-import ctypes
+import os
+from ctypes import *
+so_file = os.path.join(os.getcwd(), "gcc.so")
+
+
+my_functions = CDLL(so_file)
+
+print(type(my_functions))
+
+print(my_functions.square(10))
+
+print(my_functions.square(8))
